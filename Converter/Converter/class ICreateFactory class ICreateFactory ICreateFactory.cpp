3ad84e36e5ac4a6169e008a57e0c34e2835f2cbf
pawoPawan <44222283+pawoPawan@users.noreply.github.com>
2021-11-13 22:42:24 +0530

    Delete class ICreateFactory class ICreateFactory ICreateFactory.cpp

--- Converter/Converter/class ICreateFactory class ICreateFactory ICreateFactory.cpp
+++
@@ -1,8 +0,0 @@
-//
-//  class ICreateFactory class ICreateFactory ICreateFactory.cpp
-//  Converter
-//
-//  Created by Pawan Kumar on 04/11/21.
-//
-
-#include "class ICreateFactory class ICreateFactory ICreateFactory.hpp"
